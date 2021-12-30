83046142766fc1a042a5c0b9c832bce72a9728e8
DominiqueFlorence <ddaniell1987@gmail.com>
2021-12-29 19:30:05 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchToyData() {
+  fetch("http://localhose:3000/toys")
+  .then(response => response.json())
+  .then(toys => toys.forEach(toy => appendEachToytoDiv(toy)))
+};
+
+function appendEachToytoDiv(toy) {
+  const toyContainer = document.getElementById("toy-collection");
+  const card = document.createElement("div");
+  const h2 = document.createElement("h2");
+  const img = document.createElement("img");
+  const p = document.createElement("p");
+  const btn = document.createElement("button");
+}
+
+h2.innerHTML =toy.name;
+img.src = toy.img;
+p.innerHTML = toy.likes;
+btn.className = "like-btn";
+btn.innerHTML = "like";
+
+card.appendChild(h2);
+card.appendChild(img);
+card.appendChild(p);
+card.appendChild(btn);
+
+function likeToy(toy) {
+  let likes = toy.likes;
+  let configurationObject = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": likes + 1
+
+    }),
+  }
+  fetch(`http://localhost:3000/toys/${toy.id}`, configurationObject);
+  return likes + 1
+}
